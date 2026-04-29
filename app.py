7db694af66f009c53e83daa0b8766cd954cf8eb1
TheSpecific <42542085+KardinalFyodor@users.noreply.github.com>
2026-04-29 03:14:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, request, jsonify
-import tensorflow as tf
-import numpy as np
 
 app = Flask(**name**)
-
-model = tf.keras.models.load_model("model.h5")
 
 @app.route("/")
 def home():
@@ -12,13 +8,4 @@ return "API calisiyor"
 
 @app.route("/predict", methods=["POST"])
 def predict():
-data = request.json
-
-```
-values = np.array(data["values"]).reshape(1, -1)
-prediction = model.predict(values)
-
-return jsonify({
-    "result": prediction.tolist()
-})
-```
+return jsonify({"result": "ok"})
